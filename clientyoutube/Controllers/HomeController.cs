665d554ab4a63d31a47b3036f09284cec085db5d
berkan kömürcü <berkankomurcu@hotmail.com>
2021-10-25 17:06:47 +0300

    Add project files.

---
+++ clientyoutube/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using clientyoutube.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clientyoutube.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            var responseMessage = client.GetAsync("https://localhost:44356/api/products").Result;
+            List<Product> products = null;
+            if(responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                products = JsonConvert.DeserializeObject<List<Product>>(responseMessage.Content.ReadAsStringAsync().Result);
+            }
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new Product());
+        }
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8, "application/json");
+
+            var responseMessage = httpClient.PostAsync("https://localhost:44356/api/products",content).Result;
+
+            if(responseMessage.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Ekleme işlemi başarısız");
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var responseMessage = httpClient.GetAsync($"https://localhost:44356/api/products/{id}").Result;
+            if(responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var product = JsonConvert.DeserializeObject<Product>(responseMessage.Content.ReadAsStringAsync().Result);
+                return View(product);
+            }
+            return RedirectToAction("Index");
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            HttpClient httpClient = new HttpClient();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8, "application/json");
+            var responseMessage = httpClient.PutAsync($"https://localhost:44356/api/products/{product.Id}",content).Result;
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+             
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var responseMessage = httpClient.DeleteAsync($"https://localhost:44356/api/products/{id}").Result;
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
